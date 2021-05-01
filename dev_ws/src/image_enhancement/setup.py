69f48ff0d7626bd775668978561eda68f619b4e2
gadm21 <g.gad@nu.edu.eg>
2021-05-01 12:27:10 +0200

    init commit for GAN ros node

---
+++ dev_ws/src/image_enhancement/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+package_name = 'image_enhancement'
+
+setup(
+    name=package_name,
+    version='0.6.3',
+    packages=[package_name],
+    
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Gad Mohamed',
+    author_email='gadm43@yahoo.com',
+    
+    description='subscribing to raw frames, process with GANS, and publish to object detection and target analysis nodes.',
+    entry_points={
+        'console_scripts': [
+            'talker ='
+            'image_enhancement.publisher:main',
+            'listener ='
+            'image_enhancement.subscriber:main',
+        ],
+    },
+)
